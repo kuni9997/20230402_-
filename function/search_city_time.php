6c0e472e94bfc5b821de5f5bc1de6d6d82187f41
kuni <mitsuta.kuni999@gmail.com>
2023-04-02 22:45:55 +0900

    first

---
+++ function/search_city_time.php
@@ -0,0 +1,31 @@
+<?php
+
+function searchCityTime($city_name)
+{
+    require('config/cities.php');
+
+    foreach ($cities as $city) {
+        if ($city['name'] === $city_name) {
+            $date_time = new DateTime('', new DateTimeZone($city['time_zone']));
+            $time = $date_time->format('H:i');
+            $city['time'] = $time;
+
+            return $city;
+        }
+    }
+}
+
+// require('config/cities.php');
+
+// function searchCityTime($city_name)
+// {
+//     require('config/cities.php');
+//     foreach ($cities as $city){
+//         if ($city['name'] === $city_name){
+//             $date_time = new DateTime('',new DateTimeZone('Asia/Tokyo'));
+//             $time = $date_time->format('H:i');
+//             $city['time'] = $time;
+//             return $city;
+//         }
+//     }
+// }
